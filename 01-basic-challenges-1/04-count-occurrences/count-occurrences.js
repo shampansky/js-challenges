c881d2c01ef0fe81c0e95cf54b1ddad6437851ad
Evgeniy Shadrin <e.shadrin@aristos.pw>
2024-02-17 19:12:36 +0300

    01-04

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,16 @@
-function countOccurrences() {}
+function countOccurrences(string, char) {
+  /*
+  const regex = new RegExp(`[^${char}]`, 'g')
+  return string.replace(regex, '').length
+  */
+
+  let counter = 0;
+  for (const letter of string) {
+    if (letter === char) {
+      counter++;
+    }
+  }
+  return counter;
+}
 
 module.exports = countOccurrences;
