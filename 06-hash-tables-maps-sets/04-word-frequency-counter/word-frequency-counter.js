9133789db14e9e3c876a208129e0ba23c9279cd4
Evgeniy Shadrin <e.shadrin@aristos.pw>
2024-03-05 07:55:31 +0300

    06 :: 04

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,22 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+  const formattedString = str
+    .toLowerCase()
+    .split(/\W+/)
+    .filter((word) => word.length);
+
+  if (!formattedString.length) return new Map();
+  const mappedWords = new Map();
+  for (const word of formattedString) {
+    const currentValue = mappedWords.get(word);
+    mappedWords.set(word, currentValue ? currentValue + 1 : 1);
+  }
+
+  return mappedWords;
+}
+
+const testString =
+  'The quick brown fox jumps over the lazy dog. The dog barks, and the fox runs away.';
+
+console.log(wordFrequencyCounter(testString));
 
 module.exports = wordFrequencyCounter;
